e45eaa90340ad0731f4f436b027c5cd930f05e34
fansg666 <2680315178@qq.com>
2024-06-17 17:48:15 +0800

    完成返回参数

--- project-user/router/router.go
+++ project-user/router/router.go
@@ -1,1 +1,32 @@
 package router
+
+import "github.com/gin-gonic/gin"
+
+// Router 接口
+type Router interface {
+	Route(r *gin.Engine)
+}
+
+type RegisterRouter struct {
+}
+
+func New() *RegisterRouter {
+	return &RegisterRouter{}
+}
+
+func (*RegisterRouter) Route(ro Router, r *gin.Engine) {
+	ro.Route(r)
+}
+
+var routers []Router
+
+func InitRouter(r *gin.Engine) {
+	//rg := New()
+	//rg.Route(&user.RouterUser{}, r)
+	for _, ro := range routers {
+		ro.Route(r)
+	}
+}
+func Register(ro ...Router) {
+	routers = append(routers, ro...)
+}
