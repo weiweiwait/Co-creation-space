0e8684082da6a27891058fe83455544179ca9448
fansg666 <2680315178@qq.com>
2024-06-17 17:20:58 +0800

    完成优雅启动

--- project-user/main.go
+++ project-user/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	srv "my_project/project-common"
+)
 
+func main() {
+	r := gin.Default()
+	srv.Run(r, "project-user", ":8082")
 }
