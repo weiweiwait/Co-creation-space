08cf7227ab0c73010e602578eba1c53da6d2f2cf
fansg666 <2680315178@qq.com>
2024-06-23 21:30:28 +0800

    完成member

--- project-user/internal/dao/member.go
+++ project-user/internal/dao/member.go
@@ -12,6 +12,19 @@ type MemberDao struct {
 	conn *gorms.GormConn
 }
 
+func (m *MemberDao) FindMemberByIds(background context.Context, ids []int64) (list []*member.Member, err error) {
+	if len(ids) <= 0 {
+		return nil, nil
+	}
+	err = m.conn.Session(background).Model(&member.Member{}).Where("id in (?)", ids).First(&list).Error
+	return
+}
+
+func (m *MemberDao) FindMemberById(ctx context.Context, id int64) (mem *member.Member, err error) {
+	err = m.conn.Session(ctx).Where("id=?", id).First(&mem).Error
+	return
+}
+
 func (m *MemberDao) FindMember(ctx context.Context, account string, pwd string) (*member.Member, error) {
 	var mem *member.Member
 	err := m.conn.Session(ctx).Where("account=? and password=?", account, pwd).First(&mem).Error
@@ -26,14 +39,12 @@ func NewMemberDao() *MemberDao {
 		conn: gorms.New(),
 	}
 }
-func (m *MemberDao) FindMemberById(ctx context.Context, id int64) (mem *member.Member, err error) {
-	err = m.conn.Session(ctx).Where("id=?", id).First(&mem).Error
-	return
-}
+
 func (m *MemberDao) SaveMember(conn database.DbConn, ctx context.Context, mem *member.Member) error {
 	m.conn = conn.(*gorms.GormConn)
 	return m.conn.Tx(ctx).Create(mem).Error
 }
+
 func (m *MemberDao) GetMemberByEmail(ctx context.Context, email string) (bool, error) {
 	var count int64
 	err := m.conn.Session(ctx).Model(&member.Member{}).Where("email=?", email).Count(&count).Error
