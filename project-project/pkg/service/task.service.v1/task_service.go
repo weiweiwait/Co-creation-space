32d464c9a8612df7f012dffb69d0a55bcc40e965
fansg666 <2680315178@qq.com>
2024-06-23 21:59:09 +0800

    完成创建任务

--- project-project/pkg/service/task.service.v1/task_service.go
+++ project-project/pkg/service/task.service.v1/task_service.go
@@ -11,6 +11,7 @@ import (
 	"my_project/project-grpc/user/login"
 	"my_project/project-project/internal/dao"
 	"my_project/project-project/internal/data"
+	"my_project/project-project/internal/database"
 	"my_project/project-project/internal/database/tran"
 	"my_project/project-project/internal/repo"
 	"my_project/project-project/internal/rpc"
@@ -111,4 +112,100 @@ func (t *TaskService) MemberProjectList(co context.Context, msg *task.TaskReqMes
 		list = append(list, mpm)
 	}
 	return &task.MemberProjectResponse{List: list, Total: total}, nil
+}
+func (t *TaskService) SaveTask(ctx context.Context, msg *task.TaskReqMessage) (*task.TaskMessage, error) {
+	//1. 检查业务逻辑
+	if msg.Name == "" {
+		return nil, errs.GrpcError(model.TaskNameNotNull)
+	}
+	stageCode := encrypts.DecryptNoErr(msg.StageCode)
+	taskStages, err := t.taskStagesRepo.FindById(ctx, int(stageCode))
+	if err != nil {
+		zap.L().Error("project task SaveTask taskStagesRepo.FindById error", zap.Error(err))
+		return nil, errs.GrpcError(model.DBError)
+	}
+	if taskStages == nil {
+		return nil, errs.GrpcError(model.TaskStagesNotNull)
+	}
+	projectCode := encrypts.DecryptNoErr(msg.ProjectCode)
+	project, err := t.projectRepo.FindProjectById(ctx, projectCode)
+	if err != nil {
+		zap.L().Error("project task SaveTask projectRepo.FindProjectById error", zap.Error(err))
+		return nil, errs.GrpcError(model.DBError)
+	}
+	if project == nil || project.Deleted == model.Deleted {
+		return nil, errs.GrpcError(model.ProjectAlreadyDeleted)
+	}
+	maxIdNum, err := t.taskRepo.FindTaskMaxIdNum(ctx, projectCode)
+	if err != nil {
+		zap.L().Error("project task SaveTask taskRepo.FindTaskMaxIdNum error", zap.Error(err))
+		return nil, errs.GrpcError(model.DBError)
+	}
+	if maxIdNum == nil {
+		a := 0
+		maxIdNum = &a
+	}
+	maxSort, err := t.taskRepo.FindTaskSort(ctx, projectCode, stageCode)
+	if err != nil {
+		zap.L().Error("project task SaveTask taskRepo.FindTaskSort error", zap.Error(err))
+		return nil, errs.GrpcError(model.DBError)
+	}
+	if maxSort == nil {
+		a := 0
+		maxSort = &a
+	}
+	assignTo := encrypts.DecryptNoErr(msg.AssignTo)
+	ts := &data.Task{
+		Name:        msg.Name,
+		CreateTime:  time.Now().UnixMilli(),
+		CreateBy:    msg.MemberId,
+		AssignTo:    assignTo,
+		ProjectCode: projectCode,
+		StageCode:   int(stageCode),
+		IdNum:       *maxIdNum + 1,
+		Private:     project.OpenTaskPrivate,
+		Sort:        *maxSort + 65536,
+		BeginTime:   time.Now().UnixMilli(),
+		EndTime:     time.Now().Add(2 * 24 * time.Hour).UnixMilli(),
+	}
+	err = t.transaction.Action(func(conn database.DbConn) error {
+		err = t.taskRepo.SaveTask(ctx, conn, ts)
+		if err != nil {
+			zap.L().Error("project task SaveTask taskRepo.SaveTask error", zap.Error(err))
+			return errs.GrpcError(model.DBError)
+		}
+
+		tm := &data.TaskMember{
+			MemberCode: assignTo,
+			TaskCode:   ts.Id,
+			JoinTime:   time.Now().UnixMilli(),
+			IsOwner:    model.Owner,
+		}
+		if assignTo == msg.MemberId {
+			tm.IsExecutor = model.Executor
+		}
+		err = t.taskRepo.SaveTaskMember(ctx, conn, tm)
+		if err != nil {
+			zap.L().Error("project task SaveTask taskRepo.SaveTaskMember error", zap.Error(err))
+			return errs.GrpcError(model.DBError)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	display := ts.ToTaskDisplay()
+	member, err := rpc.LoginServiceClient.FindMemInfoById(ctx, &login.UserMessage{MemId: assignTo})
+	if err != nil {
+		return nil, err
+	}
+	display.Executor = data.Executor{
+		Name:   member.Name,
+		Avatar: member.Avatar,
+		Code:   member.Code,
+	}
+	tm := &task.TaskMessage{}
+	copier.Copy(tm, display)
+	return tm, nil
 }
