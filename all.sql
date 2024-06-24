b325a3b7be1ee75e6d9af53b8e9db0e0923ad190
fansg666 <2680315178@qq.com>
2024-06-24 15:18:09 +0800

    创建log表

--- all.sql
+++ all.sql
@@ -118,3 +118,21 @@ CREATE TABLE `ms_task`  (
                             INDEX `pcode`(`pcode`) USING BTREE,
                             INDEX `sort`(`sort`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 12363 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '任务表' ROW_FORMAT = COMPACT;
+CREATE TABLE `ms_project_log`  (
+                                   `id` bigint(0) NOT NULL AUTO_INCREMENT,
+                                   `member_code` bigint(0) NULL DEFAULT 0 COMMENT '操作人id',
+                                   `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '操作内容',
+                                   `remark` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+                                   `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT 'create' COMMENT '操作类型',
+                                   `create_time` bigint(0) NULL DEFAULT NULL COMMENT '添加时间',
+                                   `source_code` bigint(0) NULL DEFAULT 0 COMMENT '任务id',
+                                   `action_type` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '场景类型',
+                                   `to_member_code` bigint(0) NULL DEFAULT 0,
+                                   `is_comment` tinyint(1) NULL DEFAULT 0 COMMENT '是否评论，0：否',
+                                   `project_code` bigint(0) NULL DEFAULT NULL,
+                                   `icon` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+                                   `is_robot` tinyint(1) NULL DEFAULT 0 COMMENT '是否机器人',
+                                   PRIMARY KEY (`id`) USING BTREE,
+                                   INDEX `member_code`(`member_code`) USING BTREE,
+                                   INDEX `source_code`(`source_code`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5086 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '项目日志表' ROW_FORMAT = DYNAMIC;
