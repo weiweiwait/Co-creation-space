5957adba877b4713a292cfa3ea96197daad47f11
fansg666 <2680315178@qq.com>
2024-06-24 16:05:50 +0800

    添加公时

--- project-api/api/project/task.go
+++ project-api/api/project/task.go
@@ -10,6 +10,7 @@ import (
 	_ "my_project/project-api/pkg/model/user"
 	common "my_project/project-common"
 	"my_project/project-common/errs"
+	"my_project/project-common/tms"
 	"my_project/project-grpc/task"
 	"net/http"
 	"time"
@@ -318,6 +319,26 @@ func (t *HandlerTask) taskWorkTimeList(c *gin.Context) {
 	c.JSON(http.StatusOK, result.Success(tms))
 }
 
+func (t *HandlerTask) saveTaskWorkTime(c *gin.Context) {
+	result := &common.Result{}
+	var req *model.SaveTaskWorkTimeReq
+	c.ShouldBind(&req)
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	msg := &task.TaskReqMessage{
+		TaskCode:  req.TaskCode,
+		MemberId:  c.GetInt64("memberId"),
+		Content:   req.Content,
+		Num:       int32(req.Num),
+		BeginTime: tms.ParseTime(req.BeginTime),
+	}
+	_, err := TaskServiceClient.SaveTaskWorkTime(ctx, msg)
+	if err != nil {
+		code, msg := errs.ParseGrpcError(err)
+		c.JSON(http.StatusOK, result.Fail(code, msg))
+	}
+	c.JSON(http.StatusOK, result.Success([]int{}))
+}
 func NewTask() *HandlerTask {
 	return &HandlerTask{}
 }
--- project-common/tms/tms.go
+++ project-common/tms/tms.go
@@ -11,3 +11,7 @@ func FormatYMD(t time.Time) string {
 func FormatByMill(t int64) string {
 	return time.UnixMilli(t).Format("2006-01-02 15:04:05")
 }
+func ParseTime(str string) int64 {
+	parse, _ := time.Parse("2006-01-02 15:04", str)
+	return parse.UnixMilli()
+}
