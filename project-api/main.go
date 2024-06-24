f662974cf66d532e1da0365e872f55733cabfc4b
fansg666 <2680315178@qq.com>
2024-06-24 14:22:47 +0800

    增加接口访问时间日志打印

--- project-api/main.go
+++ project-api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	_ "my_project/project-api/api"
+	"my_project/project-api/api/midd"
 	"my_project/project-api/config"
 	"my_project/project-api/router"
 	srv "my_project/project-common"
@@ -10,6 +11,7 @@ import (
 
 func main() {
 	r := gin.Default()
+	r.Use(midd.RequestLog())
 	//路由
 	router.InitRouter(r)
 
