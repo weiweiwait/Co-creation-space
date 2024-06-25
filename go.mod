9aeba10ec71f746a5ce5545896c92caceced35ed
fansg666 <2680315178@qq.com>
2024-06-25 11:51:01 +0800

    开启pprof新能分析

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module my_project
 go 1.21
 
 require (
+	github.com/gin-contrib/pprof v1.5.0
 	github.com/gin-gonic/gin v1.10.0
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/golang-jwt/jwt/v4 v4.5.0
--- project-api/main.go
+++ project-api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
 	_ "my_project/project-api/api"
 	"my_project/project-api/api/midd"
@@ -16,6 +17,8 @@ func main() {
 	r.StaticFS("/upload", http.Dir("upload"))
 	//路由
 	router.InitRouter(r)
+	//开启pprof 默认的访问路径是/debug/pprof
+	pprof.Register(r)
 
 	srv.Run(r, config.C.SC.Name, config.C.SC.Addr, nil)
 }
