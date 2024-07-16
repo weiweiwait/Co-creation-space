03b26925a1d947946709eac456367d33c14693e8
fansg666 <2680315178@qq.com>
2024-07-16 14:23:03 +0800

    处理error

--- project-project/internal/dao/dao.go
+++ project-project/internal/dao/dao.go
@@ -1,6 +1,8 @@
 package dao
 
 import (
+	"errors"
+	"my_project/project-common/errs"
 	"my_project/project-project/internal/database"
 	"my_project/project-project/internal/database/gorms"
 )
@@ -12,6 +14,17 @@ type TransactionImpl struct {
 func (t *TransactionImpl) Action(f func(conn database.DbConn) error) error {
 	t.conn.Begin()
 	err := f(t.conn)
+	var bErr *errs.BError
+	if errors.Is(err, bErr) {
+		bErr = err.(*errs.BError)
+		if bErr != nil {
+			t.conn.Rollback()
+			return bErr
+		} else {
+			t.conn.Commit()
+			return nil
+		}
+	}
 	if err != nil {
 		t.conn.Rollback()
 		return err
