621b7bf5505a441fd4baf3783e0883d5ac559b25
fansg666 <2680315178@qq.com>
2024-07-17 13:42:12 +0800

    nacos远程配置文件获取

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/golang-jwt/jwt/v4 v4.5.0
 	github.com/jinzhu/copier v0.4.0
+	github.com/nacos-group/nacos-sdk-go/v2 v2.2.7
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/spf13/viper v1.19.0
 	go.etcd.io/etcd/api/v3 v3.5.12
@@ -17,10 +18,19 @@ require (
 	google.golang.org/protobuf v1.34.1
 	gorm.io/driver/mysql v1.5.7
 	gorm.io/gorm v1.25.10
+	gorm.io/plugin/dbresolver v1.5.2
 )
 
 require (
 	github.com/BurntSushi/toml v1.4.0 // indirect
+	github.com/alibabacloud-go/debug v0.0.0-20190504072949-9472017b5c68 // indirect
+	github.com/alibabacloud-go/tea v1.1.17 // indirect
+	github.com/alibabacloud-go/tea-utils v1.4.4 // indirect
+	github.com/aliyun/alibaba-cloud-sdk-go v1.61.1800 // indirect
+	github.com/aliyun/alibabacloud-dkms-gcs-go-sdk v0.2.2 // indirect
+	github.com/aliyun/alibabacloud-dkms-transfer-go-sdk v0.1.7 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
@@ -38,20 +48,27 @@ require (
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/mock v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/jmespath/go-jmespath v0.0.0-20180206201540-c2b33e8439af // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/nacos-group/nacos-sdk-go/v2 v2.2.7 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.12.2 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.32.1 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
@@ -67,12 +84,13 @@ require (
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240311132316-a219d84964c2 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240314234333-6e1732d8331c // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	gorm.io/plugin/dbresolver v1.5.2 // indirect
 )
--- project-project/config/config.go
+++ project-project/config/config.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"bytes"
 	"github.com/go-redis/redis/v8"
+	"github.com/nacos-group/nacos-sdk-go/v2/vo"
 	"github.com/spf13/viper"
 	"log"
 	"my_project/project-common/logs"
@@ -35,6 +37,7 @@ type GrpcConfig struct {
 type EtcdConfig struct {
 	Addrs []string
 }
+
 type MysqlConfig struct {
 	Username string
 	Password string
@@ -49,6 +52,7 @@ type DbConfig struct {
 	Slave      []MysqlConfig
 	Separation bool
 }
+
 type JwtConfig struct {
 	AccessExp     int64
 	RefreshExp    int64
@@ -58,28 +62,70 @@ type JwtConfig struct {
 
 func InitConfig() *Config {
 	conf := &Config{viper: viper.New()}
-	workDir, _ := os.Getwd()
-	conf.viper.SetConfigName("config")
-	conf.viper.SetConfigType("yaml")
-	////conf.viper.AddConfigPath("/etc/ms_project/user")
-	conf.viper.AddConfigPath(workDir + "/project-project/config")
-	////conf.viper.AddConfigPath(workDir)
-	// 添加当前工作目录到配置文件路径
-
-	// 添加指定目录到配置文件路径
-	//conf.viper.AddConfigPath("/home/fjw/GolandProjects/my_project/project-user/config")
-	err := conf.viper.ReadInConfig()
-	if err != nil {
-		log.Fatalln(err)
+	//先从nacos读取配置，如果读取不到 在本地读取
+	nacosClient := InitNacosClient()
+	configYaml, err2 := nacosClient.confClient.GetConfig(vo.ConfigParam{
+		DataId: "config.yaml",
+		Group:  nacosClient.group,
+		OnChange: func(namespace, group, dataId, data string) {
+			//
+			log.Printf("load nacos config changed %s \n", data)
+			err := conf.viper.ReadConfig(bytes.NewBuffer([]byte(data)))
+			if err != nil {
+				log.Printf("load nacos config changed err : %s \n", err.Error())
+			}
+			//所有的配置应该重新读取
+			conf.ReLoadAllConfig()
+		},
+	})
+	if err2 != nil {
+		log.Fatalln(err2)
 	}
-	conf.ReadServerConfig()
-	conf.InitZapLog()
-	conf.ReadGrpcConfig()
-	conf.ReadEtcdConfig()
-	conf.InitMysqlConfig()
-	conf.InitJwtConfig()
-	conf.InitDbConfig()
+	err2 = nacosClient.confClient.ListenConfig(vo.ConfigParam{
+		DataId: "config.yaml",
+		Group:  nacosClient.group,
+	})
+	if err2 != nil {
+		log.Fatalln(err2)
+	}
+	conf.viper.SetConfigType("yaml")
+	if configYaml != "" {
+		err := conf.viper.ReadConfig(bytes.NewBuffer([]byte(configYaml)))
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+	} else {
+		workDir, _ := os.Getwd()
+		conf.viper.SetConfigName("config")
+		conf.viper.SetConfigType("yaml")
+		////conf.viper.AddConfigPath("/etc/ms_project/user")
+		conf.viper.AddConfigPath(workDir + "/project-project/config")
+		////conf.viper.AddConfigPath(workDir)
+		// 添加当前工作目录到配置文件路径
+
+		// 添加指定目录到配置文件路径
+		//conf.viper.AddConfigPath("/home/fjw/GolandProjects/my_project/project-user/config")
+		err := conf.viper.ReadInConfig()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+	conf.ReLoadAllConfig()
 	return conf
+}
+
+func (c *Config) ReLoadAllConfig() {
+	c.ReadServerConfig()
+	c.InitZapLog()
+	c.ReadGrpcConfig()
+	c.ReadEtcdConfig()
+	c.InitMysqlConfig()
+	c.InitJwtConfig()
+	c.InitDbConfig()
+	//重新创建相关的客户端
+	c.ReConnRedis()
+	c.ReConnMysql()
 }
 
 func (c *Config) InitZapLog() {
@@ -132,6 +178,7 @@ func (c *Config) ReadEtcdConfig() {
 	ec.Addrs = addrs
 	c.EtcdConfig = ec
 }
+
 func (c *Config) InitMysqlConfig() {
 	mc := &MysqlConfig{
 		Username: c.viper.GetString("mysql.username"),
@@ -142,6 +189,7 @@ func (c *Config) InitMysqlConfig() {
 	}
 	c.MysqlConfig = mc
 }
+
 func (c *Config) InitJwtConfig() {
 	mc := &JwtConfig{
 		AccessSecret:  c.viper.GetString("jwt.accessSecret"),
@@ -171,3 +219,202 @@ func (c *Config) InitDbConfig() {
 	mc.Slave = slaves
 	c.DbConfig = mc
 }
+
+//package config
+//
+//import (
+//	"bytes"
+//	"github.com/go-redis/redis/v8"
+//	"github.com/nacos-group/nacos-sdk-go/v2/vo"
+//	"github.com/spf13/viper"
+//	"log"
+//	"my_project/project-common/logs"
+//	"os"
+//)
+//
+//var C = InitConfig()
+//
+//type Config struct {
+//	viper       *viper.Viper
+//	SC          *ServerConfig
+//	GC          *GrpcConfig
+//	EtcdConfig  *EtcdConfig
+//	MysqlConfig *MysqlConfig
+//	JwtConfig   *JwtConfig
+//	DbConfig    DbConfig
+//}
+//
+//type ServerConfig struct {
+//	Name string
+//	Addr string
+//}
+//
+//type GrpcConfig struct {
+//	Name    string
+//	Addr    string
+//	Version string
+//	Weight  int64
+//}
+//
+//type EtcdConfig struct {
+//	Addrs []string
+//}
+//type MysqlConfig struct {
+//	Username string
+//	Password string
+//	Host     string
+//	Port     int
+//	Db       string
+//	Name     string
+//}
+//
+//type DbConfig struct {
+//	Master     MysqlConfig
+//	Slave      []MysqlConfig
+//	Separation bool
+//}
+//type JwtConfig struct {
+//	AccessExp     int64
+//	RefreshExp    int64
+//	AccessSecret  string
+//	RefreshSecret string
+//}
+//
+//func InitConfig() *Config {
+//	conf := &Config{viper: viper.New()}
+//	nacosClient := InitNacosClient()
+//	configYaml, err2 := nacosClient.confClient.GetConfig(vo.ConfigParam{
+//		DataId: "config.yaml",
+//		Group:  nacosClient.group,
+//	})
+//	if err2 != nil {
+//		log.Fatalln(err2)
+//	}
+//	err2 = nacosClient.confClient.ListenConfig(vo.ConfigParam{
+//		DataId: "config.yaml",
+//		Group:  nacosClient.group,
+//	})
+//	if err2 != nil {
+//		log.Fatalln(err2)
+//	}
+//	conf.viper.SetConfigType("yaml")
+//	if configYaml != "" {
+//		err := conf.viper.ReadConfig(bytes.NewBuffer([]byte(configYaml)))
+//		if err != nil {
+//			log.Fatalln(err)
+//		}
+//	} else {
+//		workDir, _ := os.Getwd()
+//		conf.viper.SetConfigName("config")
+//		conf.viper.SetConfigType("yaml")
+//		////conf.viper.AddConfigPath("/etc/ms_project/user")
+//		conf.viper.AddConfigPath(workDir + "/project-project/config")
+//		////conf.viper.AddConfigPath(workDir)
+//		// 添加当前工作目录到配置文件路径
+//
+//		// 添加指定目录到配置文件路径
+//		//conf.viper.AddConfigPath("/home/fjw/GolandProjects/my_project/project-user/config")
+//		err := conf.viper.ReadInConfig()
+//		if err != nil {
+//			log.Fatalln(err)
+//		}
+//	}
+//	conf.ReadServerConfig()
+//	conf.InitZapLog()
+//	conf.ReadGrpcConfig()
+//	conf.ReadEtcdConfig()
+//	conf.InitMysqlConfig()
+//	conf.InitJwtConfig()
+//	conf.InitDbConfig()
+//	return conf
+//}
+//
+//func (c *Config) InitZapLog() {
+//	//从配置中读取日志配置，初始化日志
+//	lc := &logs.LogConfig{
+//		DebugFileName: c.viper.GetString("zap.debugFileName"),
+//		InfoFileName:  c.viper.GetString("zap.infoFileName"),
+//		WarnFileName:  c.viper.GetString("zap.warnFileName"),
+//		MaxSize:       c.viper.GetInt("maxSize"),
+//		MaxAge:        c.viper.GetInt("maxAge"),
+//		MaxBackups:    c.viper.GetInt("maxBackups"),
+//	}
+//	err := logs.InitLogger(lc)
+//	if err != nil {
+//		log.Fatalln(err)
+//	}
+//}
+//
+//func (c *Config) ReadServerConfig() {
+//	sc := &ServerConfig{}
+//	sc.Name = c.viper.GetString("server.name")
+//	sc.Addr = c.viper.GetString("server.addr")
+//	c.SC = sc
+//}
+//
+//func (c *Config) ReadGrpcConfig() {
+//	gc := &GrpcConfig{}
+//	gc.Name = c.viper.GetString("grpc.name")
+//	gc.Addr = c.viper.GetString("grpc.addr")
+//	gc.Version = c.viper.GetString("grpc.version")
+//	gc.Weight = c.viper.GetInt64("grpc.weight")
+//	c.GC = gc
+//}
+//
+//func (c *Config) ReadRedisConfig() *redis.Options {
+//	return &redis.Options{
+//		Addr:     c.viper.GetString("redis.host") + ":" + c.viper.GetString("redis.port"),
+//		Password: c.viper.GetString("redis.password"),
+//		DB:       c.viper.GetInt("redis.db"),
+//	}
+//}
+//
+//func (c *Config) ReadEtcdConfig() {
+//	ec := &EtcdConfig{}
+//	var addrs []string
+//	err := c.viper.UnmarshalKey("etcd.addrs", &addrs)
+//	if err != nil {
+//		log.Fatalln(err)
+//	}
+//	ec.Addrs = addrs
+//	c.EtcdConfig = ec
+//}
+//func (c *Config) InitMysqlConfig() {
+//	mc := &MysqlConfig{
+//		Username: c.viper.GetString("mysql.username"),
+//		Password: c.viper.GetString("mysql.password"),
+//		Host:     c.viper.GetString("mysql.host"),
+//		Port:     c.viper.GetInt("mysql.port"),
+//		Db:       c.viper.GetString("mysql.db"),
+//	}
+//	c.MysqlConfig = mc
+//}
+//func (c *Config) InitJwtConfig() {
+//	mc := &JwtConfig{
+//		AccessSecret:  c.viper.GetString("jwt.accessSecret"),
+//		AccessExp:     c.viper.GetInt64("jwt.accessExp"),
+//		RefreshExp:    c.viper.GetInt64("jwt.refreshExp"),
+//		RefreshSecret: c.viper.GetString("jwt.refreshSecret"),
+//	}
+//	c.JwtConfig = mc
+//}
+//
+//func (c *Config) InitDbConfig() {
+//	mc := DbConfig{}
+//	mc.Separation = c.viper.GetBool("db.separation")
+//	var slaves []MysqlConfig
+//	err := c.viper.UnmarshalKey("db.slave", &slaves)
+//	if err != nil {
+//		panic(err)
+//	}
+//	master := MysqlConfig{
+//		Username: c.viper.GetString("db.master.username"),
+//		Password: c.viper.GetString("db.master.password"),
+//		Host:     c.viper.GetString("db.master.host"),
+//		Port:     c.viper.GetInt("db.master.port"),
+//		Db:       c.viper.GetString("db.master.db"),
+//	}
+//	mc.Master = master
+//	mc.Slave = slaves
+//	c.DbConfig = mc
+//}
